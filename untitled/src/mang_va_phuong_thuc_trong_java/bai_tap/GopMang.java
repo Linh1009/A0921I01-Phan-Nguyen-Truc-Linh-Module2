97b5f8496a2a4cbb92a1ea1abcbe67c9f2db7d59
Linh10099 <linhdhpd02123@fpt.edu.vn>
2021-12-10 19:00:47 +0700

    Bài tập mảng và phương thức trong java

--- untitled/src/mang_va_phuong_thuc_trong_java/bai_tap/GopMang.java
+++ untitled/src/mang_va_phuong_thuc_trong_java/bai_tap/GopMang.java
@@ -1,4 +1,23 @@
 package mang_va_phuong_thuc_trong_java.bai_tap;
 
 public class GopMang {
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 3};
+        int[] array2 = {7, 9, 8};
+        int[] array3 = new int[array1.length + array2.length];
+        int pos = 0;
+        for(int i : array1){
+            array3[pos] = i;
+            pos++;
+        }
+        for (int i : array2){
+            array3[pos] = i;
+            pos++;
+        }
+        for(int i = 0;i<array3.length;i++){
+            System.out.print(array3[i] + "\t");
+        }
+
+    }
+
 }
