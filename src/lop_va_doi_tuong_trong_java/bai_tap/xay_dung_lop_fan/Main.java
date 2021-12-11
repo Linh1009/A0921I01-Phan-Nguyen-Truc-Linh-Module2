c6834e20c29c9b93592c697e48a77fe9509aa74c
Linh10099 <linhdhpd02123@fpt.edu.vn>
2021-12-11 19:19:53 +0700

    Bài tập lớp và đối tượng trong java

---
+++ src/lop_va_doi_tuong_trong_java/bai_tap/xay_dung_lop_fan/Main.java
@@ -0,0 +1,11 @@
+package lop_va_doi_tuong_trong_java.bai_tap.xay_dung_lop_fan;
+
+public class ChayChuongTrinh {
+    public static void main(String[] args) {
+        Fan f1 = new Fan(10, true, 10, "yellow");
+        Fan f2 = new Fan(5, false, 5, "blue");
+        System.out.println(f1);
+        System.out.println("----------------------------");
+        System.out.println(f2);
+    }
+}
