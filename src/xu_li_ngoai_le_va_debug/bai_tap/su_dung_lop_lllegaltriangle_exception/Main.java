dbca2467f80c5e41fdc89c84c3f3c2d853b3f4e8
Linh10099 <linhdhpd02123@fpt.edu.vn>
2022-01-21 18:08:50 +0700

    Bài tập xử lí ngoại lệ và debug

--- src/xu_li_ngoai_le_va_debug/bai_tap/su_dung_lop_lllegaltriangle_exception/Main.java
+++ src/xu_li_ngoai_le_va_debug/bai_tap/su_dung_lop_lllegaltriangle_exception/Main.java
@@ -1,5 +1,14 @@
 package xu_li_ngoai_le_va_debug.bai_tap.su_dung_lop_lllegaltriangle_exception;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Triangle triangle = null;
+        try{
+            triangle = new Triangle(2,4,3);
+        }catch (IllegalTriangleException e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        System.out.println(triangle);
+    }
 }
--- src/xu_li_ngoai_le_va_debug/bai_tap/su_dung_lop_lllegaltriangle_exception/Triangle.java
+++ src/xu_li_ngoai_le_va_debug/bai_tap/su_dung_lop_lllegaltriangle_exception/Triangle.java
@@ -1,19 +1,25 @@
 package xu_li_ngoai_le_va_debug.bai_tap.su_dung_lop_lllegaltriangle_exception;
 
 public class Triangle {
-    public int a;
-    public int b;
-    public int c;
+    private int a;
+    private int b;
+    private int c;
 
     public Triangle(int a, int b, int c) throws IllegalTriangleException{
-        this.a = a;
-        this.b = b;
-        this.c = c;
         if(a > 0 && b > 0 && c > 0){
             this.a = a;
             this.b = b;
             this.c = c;
         }
         else throw new IllegalTriangleException("Tam giác không họp lệ");
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "a=" + a +
+                ", b=" + b +
+                ", c=" + c +
+                '}';
     }
 }
